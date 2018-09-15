f85ce128428c69818d487db5f9a503fb9cf24af3
benCaffee <32526430+benCaffee@users.noreply.github.com>
2018-09-15 11:57:02 -0700

    Added "partial" instructions

--- Assets/Scripts/JoinGamePanel.cs
+++ Assets/Scripts/JoinGamePanel.cs
@@ -11,7 +11,7 @@ public class JoinGamePanel : MonoBehaviour {
 	[SerializeField]
 	private Text Info;
 
-	void Start () {
+	void Awake () {
 		mainMenu.OnPlayerSelected += SetInstructions;
 	}
 	
@@ -23,11 +23,12 @@ public class JoinGamePanel : MonoBehaviour {
 
     	if (GameManager.playerType == PlayerType.Chef) {
 		
-			Info.text = "Chef";
+			Info.text = "Complete as many orders as you can and make sure the mice don't get your food!";
 
 		} else {
 
-			Info.text = "Mouse";
+			Info.text = "Steal the Chef's ingredients and escape through the hole in the wall to score points.\n" + 
+			"WASD and you can climb walls once you get close to them.";
 		}
 	}
 }
