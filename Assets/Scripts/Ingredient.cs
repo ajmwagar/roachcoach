d902f11eae4ba7f3fa4108ecd37b82d752dab4f3
Your Name <you@example.com>
2018-09-15 01:24:58 -0700

    Orderhandler/Order/Ingriedent 1.0

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient {
+  public enum ITypes {
+    LETTUCE, TOMATO, CHEESE, HAM, WHEATBREAD, WHITEBREAD
+  }
+}
---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour {
+  public List<Ingredient> ings;
+
+  public Order(bool random){
+    if (random){
+      // TODO Implement
+    }
+  }
+
+  public Order(){
+    ings = new List<Ingredient>();
+  }
+}
