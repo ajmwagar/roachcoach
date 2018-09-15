736d33e9c89da7bcf13b11dbcdaee7b02e4ccee9
teelrc@gmail.com <teelrc@gmail.com>
2018-09-15 15:54:24 -0700

    added mouse item grabber

---
+++ Assets/Scripts/ChefItemGrabber.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefItemGrabber : MonoBehaviour
+{
+
+    public Transform attachPoint;
+    PlayerType playerType = PlayerType.Chef;
+
+    private void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    //void OnCollisionEnter(Collision other)
+    {
+
+        ItemPickUp item = other.GetComponent<ItemPickUp>();
+        if (item)
+        {
+            item.HeldBy(attachPoint, playerType);
+        }
+
+
+    }
+}
