e9831243973c52dd5e185dba0766b9d7e68c6044
benCaffee <32526430+benCaffee@users.noreply.github.com>
2018-09-16 11:19:03 -0700

    Added OrderNotification

---
+++ Assets/Scripts/OrderNotification.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderNotification : MonoBehaviour
+{
+    //public event Action NewOrderPoppedUp = delegate { };
+
+    public OrderHandler OH;
+
+    [SerializeField]
+    private TextMesh UserAndLabel;
+
+    [SerializeField]
+    private TextMesh Description;
+
+    void Awake()
+    {
+        //NewOrderPoppedUp += SetOrderNotification;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetOrderNotification()
+    {
+        UserAndLabel.text = "User: " + OH.final.user + "\nLabel: " + OH.final.label;
+        Description.text = "Desription: " + OH.final.description;
+    }
+}
