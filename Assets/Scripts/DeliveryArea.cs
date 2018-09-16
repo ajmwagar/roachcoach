a20623359363dc72897a151ef8f9905986d50755
Your Name <you@example.com>
2018-09-15 21:21:18 -0700

    Working

---
+++ Assets/Scripts/DeliveryArea.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeliveryArea : MonoBehaviour 
+{
+	public float pickupDeliveryTimeInSecs = 5;
+
+	public event Action OnOrderAdded = delegate { };
+	public event Action<Plate> OnPlatePickUp = delegate { }; 
+
+	private Dictionary<Plate, DateTime> deliveryArea;
+
+	private void Start()
+	{
+		deliveryArea = new Dictionary<Plate, DateTime>();
+	}
+
+	private void Update()
+	{
+		if(deliveryArea.Keys.Count > 0)
+		{
+			foreach(var key in deliveryArea.Keys)
+			{
+				if(deliveryArea[key] > System.DateTime.UtcNow)
+				{
+					OnPlatePickUp.Invoke(key);
+					deliveryArea.Remove(key);
+				}
+			}
+		}
+	}
+
+    void OnTriggerEnter(Collider col)
+	{
+		var plate = col.gameObject.GetComponent<Plate>();
+
+		if(plate == null)
+			return;
+		
+		var pickupTime = System.DateTime.UtcNow.AddSeconds(pickupDeliveryTimeInSecs);
+
+		if(deliveryArea.ContainsKey(plate) == false)
+		{
+			deliveryArea.Add(plate, pickupTime);
+		}
+    }
+
+    void OnTriggerExit(Collider col)
+	{
+		var plate = col.gameObject.GetComponent<Plate>();
+
+		if(plate == null)
+			return;
+		
+		var pickupTime = System.DateTime.UtcNow.AddSeconds(pickupDeliveryTimeInSecs);
+
+		if(deliveryArea.ContainsKey(plate) == false)
+		{
+			deliveryArea.Remove(plate);
+		}
+    }
+}
---
+++ Assets/Scripts/IngredientItem.cs
@@ -0,0 +1,20 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientItem : MonoBehaviour {
+  public Ingredient.ITypes type;
+
+
+  // Add more
+  void start(){
+  }
+
+  void update(){
+
+  }
+
+
+  // Return a random Ingredient
+}
