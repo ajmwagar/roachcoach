f1467913bdcfea8f53e16b09fedf40c32ed3dd7c
benCaffee <32526430+benCaffee@users.noreply.github.com>
2018-09-16 10:05:08 -0700

    Test commit

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -2,25 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Order {
-  public const int MAX_INGS = 8;
-  public const int MIN_INGS = 3;
+public class Order
+{
+    public const int MAX_INGS = 8;
+    public const int MIN_INGS = 3;
+    public List<Ingredient> ings;
 
+    public Order(bool random)
+    {
+        if (random)
+        {
+            ings = new List<Ingredient>();
 
-  public List<Ingredient> ings;
+            for (int i = 0; i < MAX_INGS; i++)
+            {
+                ings.Add(new Ingredient(true));
+            }
 
-  public Order(bool random){
-    if (random){
-      ings = new List<Ingredient>();
-
-      for (int i = 0; i < MAX_INGS; i++){
-        ings.Add(new Ingredient(true));
-      }
-
+        }
     }
-  }
 
-  public Order(){
-    ings = new List<Ingredient>();
-  }
+    public Order()
+    {
+        ings = new List<Ingredient>();
+    }
 }
