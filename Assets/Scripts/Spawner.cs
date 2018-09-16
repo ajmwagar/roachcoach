069bfcead279c9c21690b0c8188de5ed7b0382ff
teelrc@gmail.com <teelrc@gmail.com>
2018-09-16 08:53:40 -0700

    added click sound to menu (non-spacial).  Fixed tracked hand position

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,17 +6,20 @@ using UnityEngine.Networking;
 using System;
 public class Spawner : MonoBehaviour
 {
-  public Ingredient.ITypes type;
-  public GameObject obj;
-  public event Action<GameObject, Ingredient.ITypes> onbuttonpress;
-
-  public void Start()
+      public Ingredient.ITypes type;
+      public GameObject obj;
+      public event Action<GameObject, Ingredient.ITypes> onbuttonpress;
+      private AudioSource source;
+      
+      public void Start()
   {
-    var netManager = GameObject.FindObjectOfType<NetManager>();
-    netManager.RegisterPrefab(obj);
-  }
+      var netManager = GameObject.FindObjectOfType<NetManager>();
+      netManager.RegisterPrefab(obj);
+      source = GetComponent<AudioSource>();
+    }
   void OnTriggerEnter(Collider col)
   {
-    onbuttonpress.Invoke(obj, type);
+       source.Play();
+       onbuttonpress.Invoke(obj, type);
   }
 }
